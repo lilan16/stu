ef23e8bd1d08399e0f24a018084ca2c910e87f47
John Doe <JohnDoe@sina.com>
2017-03-08 00:18:44 -0800

    add folder 1

---
+++ 1/mian shi 100.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class m100
+    {
+        static void Main(string[] args)
+        {
+
+            call();
+
+        }
+
+       static void call()
+        {
+            m100 m = new m100();
+            //m.AssignmentString();
+            //m.TestSort();
+            m.SortList_Set();
+        }
+
+        //0725  sort
+        //sort list
+        void SortList_Set()
+        {
+            //SortedList<int> list=new SortedList<int> 
+            SortedSet<int> set = new SortedSet<int>();
+            int count = 20;
+
+            //set.Count = count;
+            Random r = new Random();
+            for(int i=0;i<count;i++)
+            {
+               set.Add(r.Next(100));
+            }
+            set.All((m) => { Console.Write("{0}  ",m); return true; });
+        }
+
+        void TestSort()
+        {
+             int  count= 20;
+            int[] arr=new int[count];
+            //init arr
+            Random r = new Random();
+            for(int i=0;i<count;i++)
+            {
+                arr[i] = r.Next(100);
+            }
+            Console.WriteLine("output array");
+            OutputArray(arr);
+
+            Console.WriteLine("sort array");
+            Sort(arr, arr.Length);
+
+            Console.WriteLine("output array");
+            OutputArray(arr);
+
+        }
+        void OutputArray(int[] arr)
+        {
+            for(int i=0;i<arr.Length;i++)
+            {
+                Console.Write("{0}  ",arr[i]);
+                //if(i%5==0)
+                //    Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        void Sort(int[] arr, int n)
+        {
+            int i, j, k;
+
+            for (i = 0; i < n; i++)
+            {
+                k = i;
+                for (j = i + 1; j < n; j++)
+                {
+                    if (arr[k] > arr[j])
+                        k = j;
+                }
+                if (i != k)
+                {
+                    Swap(ref arr[i], ref arr[k]);
+                }
+            }
+        }
+        //swap two number
+        void Swap(ref int i, ref int j)
+        {
+            int k;
+            k = i;
+            i = j;
+            j = k;
+        }
+
+        void BubbleSort(int[] arr, int n)
+        {
+            int i, j, k;
+            for (i = n; i > 0; i++)
+            {
+                for (j = 0; j < i; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                        Swap(ref arr[j], ref arr[j + 1]);
+                }
+            }
+
+            //for (i = 0; i < n; i++)
+            //{
+            //    for (j = 0; j < i; j++)
+            //    {
+            //        if (arr[j] > arr[j + 1])
+            //            Swap(ref arr[j], ref arr[j + 1]);
+            //    }
+            //}
+        }
+
+        //0720
+        //calculate operation hours
+        //a b point same string object, modify a
+        void AssignmentString()
+        {
+            string a = "hello", toa = a;
+            Console.WriteLine("a:{0}    toa:{1}",a,toa);
+            a = "hello new";
+            Console.WriteLine("a:{0}    toa:{1}", a, toa);
+
+            object objectA = "hello", toObjectA = objectA;
+            Console.WriteLine("objectA:{0}    toObjectA:{1}", objectA, toObjectA);
+            objectA = "hello new";
+            Console.WriteLine("objectA:{0}    toObjectA:{1}", objectA, toObjectA);
+
+            //object o = new object() { a = 1, b = 2 };
+
+        }
+        //a:hello toa:hello
+        //a:hello new toa:hello
+        //objectA:hello toObjectA:hello
+        //objectA:hello new toObjectA:hello
+
+        //singleton mode
+        //static ,  non static ?
+        class SingletonClass
+        {
+            private static SingletonClass _instance = null;
+            SingletonClass _instance_instance = _instance;
+
+            static SingletonClass()
+            {
+                
+                //else
+                //    return instance;
+            }
+            public static SingletonClass Instance
+            {
+                get
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new SingletonClass();
+                    }
+                    return _instance;
+                }
+                //set
+                //{
+                    
+                //}
+            }
+        }
+
+        //-----------------------------
+        Stack<int> sta = new Stack<int>();
+
+        void Recursion_ReverseStack()
+        {
+            int head=sta.Pop();
+            if (sta.Count == 1)
+            {
+                sta.Push(head);
+            }
+            else
+            {
+                Recursion_ReverseStack();
+            }
+
+        }
+
+
+        StringBuilder bui = new StringBuilder();
+        int fibonacci(int n)
+        {
+            //Console.WriteLine("n:{0}",n);
+            //System.Diagnostics.Debug.Close();
+            //System.Diagnostics.Trace.TraceInformation("n:{0}"+n.ToString());
+            //very slow
+            bui.AppendFormat("n:{0}    ",n.ToString());
+
+            if (n == 0)
+            {
+                bui.Append(Environment.NewLine);
+                return 0;
+            }
+            else if (n == 1)
+                return 1;
+            else
+                return fibonacci(n - 2) + fibonacci(n - 1);
+        }
+
+        void CallFibonacci()
+        {
+            int n = 20;
+            m100 p = new m100();
+
+            Console.WriteLine("n=" + n.ToString());
+            Console.WriteLine("result=" + p.fibonacci(n));
+
+            Console.WriteLine();
+            Console.WriteLine(p.bui.ToString());
+        }
+    }
+}
---
+++ 1/~vsE73C.sql
@@ -0,0 +1,165 @@
+﻿exec sp_databases;
+exec sp_tables; 
+select * from sys.tables
+show sp_databases status
+show create procedure sp_databases
+
+if (object_id('proc_getMovieRecord','p')is not null)
+drop proc proc_getMovieRecord
+go
+create proc proc_getMovieRecord
+as
+select top 1 * from dbo.moviedbs
+
+exec proc_getMovieRecord
+
+if (object_id('proc_getMovieRecordArgs','p')is not null)
+drop proc proc_getMovieRecordArgs
+go
+create proc proc_getMovieRecordArgs(@topCount int =1,@outAllCount int output)
+as
+select top @topCount * from dbo.moviedbs
+select count
+go
+
+insert into dbo.moviedbs(id,title,director,date) values(5,'5','5','2016-07-11')
+
+
+----------------------------------------------
+
+ --master db
+select * from dbo.msreplication_options
+--select * from dbo.spt_fallback_usg
+--select * from dbo.spt_fallback_db
+--select * from dbo.spt_fallback_dev
+select * from dbo.spt_monitor
+select * from sys.trace_xe_action_map
+select * from sys.trace_xe_event_map
+
+
+----------------------------------
+
+
+--trigger
+
+if(OBJECT_ID('tgr_afterInsert_TitleAdmin','tr') is not null)
+drop trigger tgr_afterInsert_TitleAdmin
+go
+
+create trigger tgr_afterInsert_TitleAdmin
+on dbo.moviedbs
+after insert
+as
+
+declare @id int,@title varchar(20),@director varchar(20),@time datetime 
+
+select @id=id,@title=title,@director=director,@time = date  from inserted
+if @title= 'admin'
+begin
+	raisError('title is "admin" can not insert',16,11);
+	rollback tran;
+	print 'have rollback'
+end
+
+update title ,ok
+update dbo.moviedbs set title=@title+'_trigger' where id=@id
+print 'have updated'
+
+out info,ok
+print 'add record:'
+print @title
+print @director 
+print @time
+
+if @title='ttt'
+raiserror('title = ""',16,8)
+rollback
+
+begin
+
+declare @title
+select @title=title from inserted
+if @title='ttt'
+raiserror('title = ""',16,8)
+rollback
+
+end 
+go
+
+
+-- test
+
+--select count(*) from dbo.MovieDBs
+
+--select getdate() 
+
+--select * from dbo.MovieDBs where title is not null
+
+--insert into dbo.moviedbs(id,title,director,date) values(6,'6','5',GETDATE())
+
+--show proc status
+--exec sp_helptext Procedure_Name
+--insert into dbo.moviedbs(id,title,director,date) values(10,'admin22','5',GETDATE())
+
+
+------------------------------------------
+--0721 update
+--  ~vs af6e.sql
+
+ select * from move
+select * from sys.objects
+
+select * from sysphfg
+select * from dbo.EventNotificationErrorsQueue;
+select * from sys.ServiceBrokerQueue;
+select * from sys.messages;
+
+
+select * from dbo.moviedbs
+
+declare @id int,
+declare @title varchar
+set @id=3
+
+select @title=title from dbo.moviedbs where id =@id
+update dbo.moviedbs set title='55' ,director='5555'  where id = 5
+
+insert into dbo.moviedbs(id,title,director,date) values(5,'5','5','2016-07-11')
+insert into dbo.moviedbs(id,title,director,date) values(6,'6','5','2016-07-11')
+
+delete dbo.moviedbs where id>=5
+select * into moviedbs_backup
+from moviedbs
+
+create database myDB
+select * from dbo.spt_monitor
+
+exec getdate()
+
+=============
+sql query 1
+select * from sys.triggers;
+
+disable trigger afterInsert on dbo.moviedbs;
+disable trigger tgr_afterInsert_updatetitle on dbo.moviedbs;
+disable trigger tgr_afterInsert_titleadmin on dbo.moviedbs
+--disable trigger tgr_moviedbs_InsteadUpdate on dbo.moviedbs
+
+update dbo.moviedbs set title='adminABC_Trigger' where id =10
+===================
+
+sql query 2
+if(OBJECT_ID('tgr_moviedbs_InsteadUpdate','tr') is not null)
+drop trigger tgr_moviedbs_InsteadUpdate
+go
+
+create trigger tgr_moviedbs_InsteadUpdate
+on dbo.moviedbs
+instead of update
+as
+
+declare @id int,@title varchar(20),@director varchar(20),@time datetime 
+
+select @id=id,@title=title,@director=director,@time = date  from inserted
+raisError('tgr_moviedbs_InsteadUpdate 触发',16,10);
+print 'instead of update'
